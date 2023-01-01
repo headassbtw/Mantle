fd8c6256ad1b70d446b409a586841be4f7b63d8c
headassbtw <cooldood909@gmail.com>
2023-01-01 03:27:46 -0800

    more texture research

--- include/core/rtti_types/texture.hpp
+++ include/core/rtti_types/texture.hpp
@@ -3,10 +3,16 @@
 
 struct rtti_Texture{
   char begin[72]; //full of 0xEE
-  char unk1[26]; //lmao kinda just lazy
+  //char unk0[22 - 27]; //LENGTH VARIES OF BULLSHIT!
+  char unk1[4]; //likely blank
 
   uint32_t res0; // not sure which these are (height or width)
   uint32_t res1;
+
+  char unk2[8]; //idk
+
+  uint32_t mipmap_count; //JUST SPECULATION, is 11 when res is 1024 (proper mipmap count for that res)
+
 
   char end[50]; //full of 0x50
 };
